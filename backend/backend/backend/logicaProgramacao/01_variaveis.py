d821042d8e558d65ae10967db4a34ccdeabc26b2
jaqueline spessia <jaquespessiaa@gmail.com>
2025-05-21 11:47:54 -0300

    Iniciando Logica Programacao

---
+++ backend/backend/backend/logicaProgramacao/01_variaveis.py
@@ -0,0 +1,22 @@
+nome= "Jaqueline"
+idade= 17
+salario= 56.987
+legal= True
+
+print("nome",nome)
+print("idade",idade)
+print("salario",salario)
+print("é legal?",legal)
+
+name= "Charlie"
+animal= "gato"
+peso= 82.5
+raça= "srd"
+temperamentoAgradavel= False
+
+print("Name:", name)
+print("Qual o animal?", animal)
+print("Quanto pesa em kg?", peso)
+print("Qual a raça?", raça)
+print("Seu temperamento é agradavel?", temperamentoAgradavel)
+
---
+++ backend/backend/backend/logicaProgramacao/02_opercoesMatematicas.py
@@ -0,0 +1,24 @@
+a = 10
+b = 15
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+
+print("Soma de a + b é:", soma)
+print("subtracao", subtracao)
+print("multiplicacao", multiplicacao)
+print("divisao", divisao)
+
+
+num1 = 56
+num2 = 75
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+
+print("Soma de num1 + num2 é:", soma)
+print("subtracao", subtracao)
+print("multiplicacao", multiplicacao)
+print("divisao", divisao)
