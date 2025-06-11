5870f11116e78266c7c3189138fc7b26c5d5358a
jaqueline <jaquespessiaa@gmail.com>
2025-06-11 11:50:35 -0300

    atividade

---
+++ backend/backend/backend/logicaProgramacao/04_lacos.py
@@ -0,0 +1,18 @@
+# For
+for i in range(5):
+    print("Contando:", i)
+
+# While
+    senha = ""
+    while senha != "Spessia":
+        senha = input("Digite a senha: ")
+        print("Senha Incorreta.")
+
+print("Acesso liberado.")
+
+
+
+for i in range(1,10):
+    print("Somando" , i)
+
+
---
+++ backend/backend/backend/logicaProgramacao/05_funcoes.py
@@ -0,0 +1,25 @@
+#def Charlie (nome):
+# return print ("Bem vindo, " + nome +"!!!")
+#Charlie("Pompom")
+
+
+#def calcular_media(numero1, numero2):
+
+ #  media = (numero1 + numero2) / 2
+ #  return media
+
+
+#print(calcular_media(13, 7))
+ 
+
+
+def numeropar(numero):
+    if (numero%2==0):
+        print("seu noumero é par!")
+    else:
+        print("Seu numero é ímpar!")
+
+        numeropar (16)
+
+
+
