0bb966468fa5aa88e244e8bcd18c0ede81a28ad5
jaqueline spessia <jaquespessiaa@gmail.com>
2025-06-25 11:57:39 -0300

    atividades1a5

---
+++ backend/backend/backend/logicaProgramacao/06_atividade.py
@@ -0,0 +1,28 @@
+#def contar_ate_dez ():
+  
+ #   for i in range (1, 11):
+ #       print(i)
+
+#contar_ate_dez()
+
+# nome = input("Digite o nome: ")
+
+#quantidade = int(input("Digite a quantidade de vezes que quer imprimir o nome: "))
+
+#for i in range(quantidade):
+  #  print(nome)
+     
+#numero = int(input("Digite um numero: "))
+#for i in range (1, 11):
+ ##   resultado = numero * i
+  #  print(f"{numero} x {i} = {resultado}")
+
+#for num in range (1, 21):
+  ##  if num % 2 ==0:
+  #      print(num)
+
+soma = 0 
+for i in range (5):
+    numero = float(input(f"Digite o numero {i+1}: "))
+    soma += numero 
+    print(f"A soma dos 5 numeros é: {soma} ")
