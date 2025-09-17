64b5a25f905a989b978c83880382199809212be5
jaqueline <jaquespessiaa@gmail.com>
2025-09-17 11:11:56 -0300

    django

---
+++ gerenciador_web/config/tarefas/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+#blank-branco null-nulo
+#auto_now_add - adicionar automaticamente
+
+class Tarefa (models.Model):
+
+    titulo = models.CharField (max_length = 200)
+    descricao = models.TextField (blank=True, null=True) 
+    data_criacao = models.DateTimeField (auto_now_add=True)  
+    concluida = models.BooleanField (default=False)
+
+    #exibir titulo por padrão
+    def __str__(self):
+        return self.titulo
